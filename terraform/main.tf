0554a379e1148ae25b70396511cd1321e9ff6689
Pieter Tolsma <epieter.tolsma@gmail.com>
2022-05-18 11:20:00 +0200

    Working deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,37 @@ provider "google" {
   region  = "europe-west1"
 }
 
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
 module "gke_auth" {
-  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
   project_id   = var.project_id
   location     = module.gke.location
   cluster_name = module.gke.name
 }
+
 resource "local_file" "kubeconfig" {
   content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
+  filename = "${path.module}/kubeconfig"
 }
+
+provider "kubernetes" {
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  host                   = module.gke_auth.host
+  token                  = module.gke_auth.token
+}
+
+provider "docker" {
+  host = "ghcr.io/Fastjur"
+}
+
 module "gcp-network" {
   source       = "terraform-google-modules/network/google"
   version      = "> 2.5"
---
+++ terraform/pods.tf
@@ -0,0 +1,103 @@
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "backend-service"
+    namespace="prod"
+  }
+  spec {
+    selector = {
+      app = kubernetes_pod.example.metadata.0.labels.app
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 8080
+      target_port = 8080
+    }
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_pod" "frontend" {
+  metadata {
+    name = "inference-frontend"
+    namespace = "prod"
+    labels = {
+        App = "inference-frontend"
+    }
+  }
+
+  spec {
+    container {
+      image = "ghcr.io/fastjur/frontend:latest"
+      name  = "inference-frontend"
+
+      env {
+        name  = "environment"
+        value = "prod"
+      }
+
+      port {
+        container_port = 8080
+      }
+
+    }
+
+    # dns_config {
+    #   nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+    #   searches    = ["example.com"]
+
+    #   option {
+    #     name  = "ndots"
+    #     value = 1
+    #   }
+
+    #   option {
+    #     name = "use-vc"
+    #   }
+    # }
+
+    # dns_policy = "None"
+  }
+}
+
+
+resource "kubernetes_pod" "example" {
+  metadata {
+    name = "inference-backend"
+    namespace = "prod"
+    labels = {
+        app = "inference-backend"
+    }
+  }
+
+  spec {
+    container {
+      image = "ghcr.io/fastjur/inference:latest"
+      name  = "inference-backend"
+
+      env {
+        name  = "environment"
+        value = "prod"
+      }
+
+      port {
+        container_port = 8080
+      }
+    }
+
+    # dns_config {
+    #   nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+    #   searches    = ["example.com"]
+
+    #   option {
+    #     name  = "ndots"
+    #     value = 1
+    #   }
+
+    #   option {
+    #     name = "use-vc"
+    #   }
+    # }
+
+    # dns_policy = "None"
+  }
+}
