555cd816e5856f6bcc717e5aa9ffa55c9502da74
svenvdvoort <svenvdvoort@protonmail.com>
2022-05-30 15:14:26 +0200

    Small optimization in predicting library

--- src/common/predicting.py
+++ src/common/predicting.py
@@ -1,9 +1,3 @@
 def predict(preprocessed_sentence, model, tags):
     prediction = model.predict(preprocessed_sentence)
-    
-    # TODO optimize: output = tags[prediction[0]]
-    output = []
-    for i, p in enumerate(prediction[0]):
-        if p == 1:
-            output.append(tags[i])
-    return output
+    return list(tags[prediction[0] == 1])
