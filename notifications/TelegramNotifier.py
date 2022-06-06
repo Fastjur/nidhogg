fb4ad1f98c56ff2f1617b56720023851be461a6a
svenvdvoort <svenvdvoort@protonmail.com>
2022-06-06 11:03:39 +0200

    Create low precision Telegram notification type

--- notifications/TelegramNotifier.py
+++ notifications/TelegramNotifier.py
@@ -27,23 +27,37 @@ class TelegramNotifier:
 
     @staticmethod
     def create_relevance_buttons(message_type):
-        if message_type == 'pr_opened':
-            return [
-                {
-                    'text': 'Relevant',
-                    'url': 'http://127.0.0.1:3001/vote?category={}&id={}&vote=positive'.format('PR', message_type)
-                },
-                {
-                    'text': 'Irrelevant',
-                    'url': 'http://127.0.0.1:3001/vote?category={}&id={}&vote=negative'.format('PR', message_type)
-                },
-            ]
+        default_buttons = [
+            {
+                'text': 'Relevant',
+                'url': 'http://127.0.0.1:3001/vote?category={}&id={}&vote=positive'.format('PR', message_type)
+            },
+            {
+                'text': 'Irrelevant',
+                'url': 'http://127.0.0.1:3001/vote?category={}&id={}&vote=negative'.format('PR', message_type)
+            },
+        ]
+        if message_type == 'pr_opened' or message_type == 'low_precision':
+            return default_buttons
         raise Exception('Unknown message type: {}'.format(message_type))
 
     def notify_pr_opened(self, pr_author, pr_title, pr_url):
         with open('templates/pr_opened.txt') as f:
             message_type = 'pr_opened'
             message_text = ''.join(f.readlines()).format(pr_author, pr_title, pr_url, message_type)
+            print("📩 Sending message:")
+            print(message_text)
+
+            inline_keyboard_markup = [
+                    [{'text': 'View on GitHub', 'url': pr_url}],
+                    self.create_relevance_buttons(message_type)
+            ]
+            self.notify(message_text, inline_keyboard_markup)
+
+    def notify_pr_opened(self, pr_author, pr_title, pr_url, model_precision):
+        with open('templates/low_precision.txt') as f:
+            message_type = 'low_precision'
+            message_text = ''.join(f.readlines()).format(model_precision, pr_author, pr_title, pr_url, message_type)
             print("📩 Sending message:")
             print(message_text)
 
